418d150aa9a7ebcd4a87c9989fd7170fa0de071d
nanakyeong <dlskrud468@naver.com>
2025-09-01 12:58:17 +0900

    feat : SpringSecurity 생성

--- src/main/java/com/hanjum/newshanjumapi/jwt/SecurityConfig.java
+++ src/main/java/com/hanjum/newshanjumapi/jwt/SecurityConfig.java
@@ -14,6 +14,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
                                 "/api/**",
